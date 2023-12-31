e045500ac57d20f858feccb64e17b040a7b83d1d
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-12-31 21:08:52 +0330

    added: the control flow graph

--- ruse.c
+++ ruse.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
   int version;
@@ -81,3 +82,98 @@ PhiInstruction createPhiInstruction(SSAOperand result, SSAOperand operand1,
   phiInstruction.operand2 = operand2;
   return phiInstruction;
 }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct BasicBlock;
+
+
+typedef struct {
+  struct BasicBlock* source;
+  struct BasicBlock* destination;
+} CFGEdge;
+
+
+typedef struct BasicBlock {
+  
+  Instruction* instructions;
+  int numInstructions;
+
+  
+  CFGEdge* successors;  
+  int numSuccessors;
+
+  CFGEdge* predecessors;  
+  int numPredecessors;
+
+  int blockID;  
+} BasicBlock;
+
+
+typedef struct {
+  BasicBlock* basicBlocks;
+  int numBlocks;
+} ControlFlowGraph;
+
+
+BasicBlock createBasicBlock(void) {
+  BasicBlock block;
+  
+  block.instructions = NULL;
+  block.numInstructions = 0;
+  block.successors = NULL;
+  block.numSuccessors = 0;
+  block.predecessors = NULL;
+  block.numPredecessors = 0;
+  block.blockID = -1;  
+  return block;
+}
+
+
+ControlFlowGraph createControlFlowGraph(void) {
+  ControlFlowGraph cfg;
+  
+  cfg.basicBlocks = NULL;
+  cfg.numBlocks = 0;
+  return cfg;
+}
+
+
+void addEdge(BasicBlock* source, BasicBlock* destination) {
+  
+  source->successors = realloc(source->successors, (source->numSuccessors + 1) * sizeof(CFGEdge));
+  source->successors[source->numSuccessors].source = source;
+  source->successors[source->numSuccessors].destination = destination;
+  source->numSuccessors++;
+
+  
+  destination->predecessors = realloc(destination->predecessors, (destination->numPredecessors + 1) * sizeof(CFGEdge));
+  destination->predecessors[destination->numPredecessors].source = source;
+  destination->predecessors[destination->numPredecessors].destination = destination;
+  destination->numPredecessors++;
+}
+
+
+ControlFlowGraph initializeCFG(void) {
+  ControlFlowGraph cfg;
+  cfg.basicBlocks = malloc(2 * sizeof(BasicBlock));  
+
+  
+  cfg.basicBlocks[0] = createBasicBlock();
+  cfg.basicBlocks[1] = createBasicBlock();
+
+  
+  cfg.basicBlocks[0].blockID = 0;
+  cfg.basicBlocks[1].blockID = 1;
+
+  
+  addEdge(&cfg.basicBlocks[0], &cfg.basicBlocks[1]);
+
+  cfg.numBlocks = 2;
+  return cfg;
+}
+
+
