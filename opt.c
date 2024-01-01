ce545565433a9b3b663c4bba5bc25b093f7c95d6
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-01 08:41:50 +0330

    added: constant folding

--- opt.c
+++ opt.c
@@ -1,7 +1,6 @@
 #include "ssa.h"
 #include "cfg.h"
 
-
 void optimizeConstantFolding(IRFunction *irFunction) {
     for (int i = 0; i < irFunction->numBlocks; i++) {
         InstructionBlock *block = &irFunction->instructionBlocks[i];
@@ -18,7 +17,79 @@ void optimizeConstantFolding(IRFunction *irFunction) {
                                       instr->irInstruction.operand2.value;
                         break;
 
-                    default:
+                    case IR_SUB:
+                        resultValue = instr->irInstruction.operand1.value -
+                                      instr->irInstruction.operand2.value;
+                        break;
+
+                    case IR_MUL:
+                        resultValue = instr->irInstruction.operand1.value *
+                                      instr->irInstruction.operand2.value;
+                        break;
+
+                    case IR_DIV:
+                        if (instr->irInstruction.operand2.value != 0) {
+                            resultValue = instr->irInstruction.operand1.value /
+                                          instr->irInstruction.operand2.value;
+                        } else {
+                            // Division by zero, handle appropriately (set result to some default value, issue a warning, etc.)
+                        }
+                        break;
+
+                    case IR_MOD:
+                        if (instr->irInstruction.operand2.value != 0) {
+                            resultValue = instr->irInstruction.operand1.value %
+                                          instr->irInstruction.operand2.value;
+                        } else {
+                            // Modulus by zero, handle appropriately
+                        }
+                        break;
+
+                    case IR_EQ:
+                        resultValue = (instr->irInstruction.operand1.value == instr->irInstruction.operand2.value) ? 1 : 0;
+                        break;
+
+                    case IR_NE:
+                        resultValue = (instr->irInstruction.operand1.value != instr->irInstruction.operand2.value) ? 1 : 0;
+                        break;
+
+                    case IR_GT:
+                        resultValue = (instr->irInstruction.operand1.value > instr->irInstruction.operand2.value) ? 1 : 0;
+                        break;
+
+                    case IR_GE:
+                        resultValue = (instr->irInstruction.operand1.value >= instr->irInstruction.operand2.value) ? 1 : 0;
+                        break;
+
+                    case IR_LT:
+                        resultValue = (instr->irInstruction.operand1.value < instr->irInstruction.operand2.value) ? 1 : 0;
+                        break;
+
+                    case IR_LE:
+                        resultValue = (instr->irInstruction.operand1.value <= instr->irInstruction.operand2.value) ? 1 : 0;
+                        break;
+
+                    case IR_AND:
+                        resultValue = (instr->irInstruction.operand1.value && instr->irInstruction.operand2.value) ? 1 : 0;
+                        break;
+
+                    case IR_OR:
+                        resultValue = (instr->irInstruction.operand1.value || instr->irInstruction.operand2.value) ? 1 : 0;
+                        break;
+
+                    case IR_BITWISE_AND:
+                        resultValue = instr->irInstruction.operand1.value & instr->irInstruction.operand2.value;
+                        break;
+
+                    case IR_BITWISE_OR:
+                        resultValue = instr->irInstruction.operand1.value | instr->irInstruction.operand2.value;
+                        break;
+
+                    case IR_BITWISE_XOR:
+                        resultValue = instr->irInstruction.operand1.value ^ instr->irInstruction.operand2.value;
+                        break;
+
+                   default:
                         break;
                 }
 
@@ -32,3 +103,4 @@ void optimizeConstantFolding(IRFunction *irFunction) {
         }
     }
 }
+ 
