462b289e67a31a287e6e5337050a31b414df6114
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-01 07:54:26 +0330

    added: more factory functions

--- ssa.c
+++ ssa.c
@@ -11,11 +11,42 @@ typedef enum {
   IR_SUB,
   IR_MUL,
   IR_DIV,
+  IR_MOD,      
+  IR_NEG,      
+  IR_AND,      
+  IR_OR,       
+  IR_XOR,      
+  IR_NOT,      
+  IR_LSHIFT,   
+  IR_RSHIFT,   
+  IR_LT,       
+  IR_LE,       
+  IR_GT,       
+  IR_GE,       
+  IR_EQ,       
+  IR_NE,       
+  IR_GOTO,     
+  IR_IF,       
+  IR_LABEL,    
+  IR_RETURN,   
+  IR_CALL,     
+  IR_PARAM,    
+  IR_NOP,      
+  IR_CAST,  
+  IR_SWITCH,   
+  IR_CASE,     
+  IR_DEFAULT,  
+  IR_BREAK,    
+  IR_CONTINUE, 
+  IR_DO_WHILE, 
+  IR_WHILE,    
+  IR_FOR,      
+  IR_PHI,  
   IR_ASSIGN,
   IR_LOAD,
   IR_STORE,
-  IR_PHI,
 } IROpcode;
+
 
 typedef struct {
   int isConstant;
@@ -68,26 +99,82 @@ typedef struct {
   int numFunctions;
 } IRProgram;
 
-Instruction *createIRInstruction(IROpcode opcode, SSAOperand result,
-                                  IROperand operand1, IROperand operand2) {
-  Instruction *instruction = zalloc(sizeof(Instruction));
-  instruction->kind = INST_IR;
-  instruction->irInstruction.opcode = opcode;
-  instruction->irInstruction.result = result;
-  instruction->irInstruction.operand1 = operand1;
-  instruction->irInstruction.operand2 = operand2;
-  return instruction;
+
+SSAOperand createSSAOperand(int version, int index) {
+    SSAOperand operand;
+    operand.version = version;
+    operand.index = index;
+    return operand;
 }
 
-Instruction *createPhiInstruction(SSAOperand result, SSAOperand operand1,
+
+IROperand createIROperand(int isConstant, int value) {
+    IROperand operand;
+    operand.isConstant = isConstant;
+    operand.value = value;
+    return operand;
+}
+
+
+IRInstruction createIRInstruction(IROpcode opcode, SSAOperand result,
+                                  IROperand operand1, IROperand operand2) {
+    IRInstruction instruction;
+    instruction.opcode = opcode;
+    instruction.result = result;
+    instruction.operand1 = operand1;
+    instruction.operand2 = operand2;
+    return instruction;
+}
+
+
+PhiInstruction createPhiInstruction(SSAOperand result, SSAOperand operand1,
                                     SSAOperand operand2) {
-  Instruction *instruction = zalloc(sizeof(Instruction));
-  instruction->kind = INST_PHI;
-  instruction->phiInstruction.opcode = IR_PHI;
-  instruction->phiInstruction.result = result;
-  instruction->phiInstruction.operand1 = operand1;
-  instruction->phiInstruction.operand2 = operand2;
-  return phiInstruction;
+    PhiInstruction instruction;
+    instruction.opcode = IR_PHI;
+    instruction.result = result;
+    instruction.operand1 = operand1;
+    instruction.operand2 = operand2;
+    return instruction;
+}
+
+
+Instruction createInstruction(InstructionKind kind, PhiInstruction phiInstr, IRInstruction irInstr) {
+    Instruction instruction;
+    instruction.kind = kind;
+    
+    if (kind == INST_PHI) {
+        instruction.phiInstruction = phiInstr;
+    } else {
+        instruction.irInstruction = irInstr;
+    }
+    
+    return instruction;
+}
+
+
+InstructionBlock createInstructionBlock(Instruction *instructions, CFGBlock *cfgBlock, int numInstructions) {
+    InstructionBlock block;
+    block.instructions = instructions;
+    block.cfgBlock = cfgBlock;
+    block.numInstructions = numInstructions;
+    return block;
+}
+
+
+IRFunction createIRFunction(InstructionBlock *instructionBlocks, CFGBlock *entryCFGBlock, int numBlocks) {
+    IRFunction irFunc;
+    irFunc.instructionBlocks = instructionBlocks;
+    irFunc.entryCFGBlock = entryCFGBlock;
+    irFunc.numBlocks = numBlocks;
+    return irFunc;
+}
+
+
+IRProgram createIRProgram(IRFunction *functions, int numFunctions) {
+    IRProgram program;
+    program.functions = functions;
+    program.numFunctions = numFunctions;
+    return program;
 }
 
 
