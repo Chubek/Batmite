6eba0e8ec63ac015fb323899a58f571e42a9b68c
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-02 10:12:38 +0330

    init: directed acyclic graph

---
+++ dag.c
@@ -0,0 +1,57 @@
+
+typedef struct DAGNode {
+  IROpcode opcode;
+  SSAOperand result;
+  IROperand operand1;
+  IROperand operand2;
+  struct DAGNode *left;
+  struct DAGNode *right;
+} DAGNode;
+
+typedef enum {
+  INST_DAG_NODE,
+  INST_DAG_PHI,
+} DAGInstructionKind;
+
+typedef struct {
+  DAGInstructionKind kind;
+  union {
+    PhiInstruction phiDAGInstruction;
+    DAGNode *dagNode;
+  };
+} DAGInstruction;
+
+DAGNode *createDAGNode(IROpcode opcode, SSAOperand result, IROperand operand1,
+                       IROperand operand2, DAGNode *left, DAGNode *right) {
+  DAGNode *node = (DAGNode *)zalloc(sizeof(DAGNode));
+  node->opcode = opcode;
+  node->result = result;
+  node->operand1 = operand1;
+  node->operand2 = operand2;
+  node->left = left;
+  node->right = right;
+  return node;
+}
+
+DAGInstruction createDAGInstruction(DAGInstructionKind kind,
+                                    PhiInstruction phiInstr, DAGNode *dagNode) {
+  DAGInstruction instruction;
+  instruction.kind = kind;
+
+  if (kind == INST_DAG_PHI) {
+    instruction.phiInstruction = phiInstr;
+  } else {
+    instruction.dagNode = dagNode;
+  }
+
+  return instruction;
+}
+
+IRFunction createDAGIrFunction(InstructionBlock *instructionBlocks,
+                               CFGBlock *entryCFGBlock, int numBlocks) {
+  IRFunction irFunc;
+  irFunc.instructionBlocks = instructionBlocks;
+  irFunc.entryCFGBlock = entryCFGBlock;
+  irFunc.numBlocks = numBlocks;
+  return irFunc;
+}
