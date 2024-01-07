bd80fddd6f53657961e0e78e3d7e3e18df7fa4aa
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-08 02:15:09 +0330

    progress: in the tree preprocessor

--- treepp.pl
+++ treepp.pl
@@ -3,57 +3,61 @@
 use strict;
 use warnings;
 
-# Read the input text from STDIN
-my $input_text;
-{
-    local $/;
-    $input_text = <STDIN>;
+my $PREFIX = "TREEPP";
+
+while (<STDIN>) {
+    chomp;  # Remove newline character
+    my $processed_line = process_line($_);
+    print "$processed_line\n";
 }
 
-# Process the input text
-my $processed_text = process_text($input_text);
+sub process_line {
+    my ($line) = @_;
 
-# Print the processed text
-print $processed_text;
-
-sub process_text {
-    my ($text) = @_;
-
-    # Regular expression for matching the tree declarations
     my $pattern = qr{
-        %tree\s*
+        ^%tree\s*
         (?:%static|%heap)?\s*
         %name\s+(\w+)\s*
-        (?:(%parent\s+(\w+)\s*)?)?
+        (?:(?:%parent\s+(\w+)\s*)?)?
         %value\s+([^\s%]+)\s*
-        (?:(%left\s+(\w+)\s*)?)?
-        (?:(%right\s+(\w+)\s*)?)?
-        \.
+        (?:(?:%left\s+(\w+)\s*)?)?
+        (?:(?:%right\s+(\w+)\s*)?)?$
     }sx;
 
-    # Replace occurrences of the specified syntax with C variable initializations
-    $text =~ s/$pattern/instantiate_tree("$1", "$2", "$3", "$4", "$5", "$6", "$7", "$8")/eg;
+    $line =~ s/$pattern/instantiate_tree("$1", "$2", "$3", "$4", "$5", "$6", "$7", "$8")/e;
 
-    # Return the processed text
-    return $text;
+    return $line;
+}
+
+sub handle_default {
+    my ($variable, $default) = @_;
+    return defined($variable) && $variable =~ /\S/ ? $variable : $default;
+}
+
+sub substitute_storage {
+    my ($storage) = @_;
+    return $storage eq "%static" ? "$PREFIX" . "_STATIC" : "";
+}
+
+sub substitute_type {
+    my ($storage) = @_;
+    return $storage eq "%heap" ? "$PREFIX" . "_PTR" : "$PREFIX" . "_NONPTR";
+}
+
+sub substitute_initfunc {
+    my ($storage) = @_;
+    return $storage eq "%heap" ? "$PREFIX" . "_ALLOC" : "$PREFIX" . "_NONALLOC";
 }
 
 sub instantiate_tree {
     my ($name, $storage, $parent, $value, $left, $right) = @_;
 
-    # Determine the variable type and storage specifier
-    my ($type, $storage_specifier) = ($storage eq '%heap') ? ('TREEPP_PTR', 'TREEPP_ALLOC') : ('TREEPP_STATIC', 'TREEPP_NOALLOC');
+    $storage = handle_default($storage, "%heap");
+    $parent  = handle_default($parent, "NULL");
+    $left    = handle_default($left, "NULL");
+    $right   = handle_default($right, "NULL");
 
-    # Generate the variable initialization code
-    my $initialization_code = <<INIT_CODE;
-$type $storage_specifier $name = TREEPP_INIT(
-    $parent,
-    $value,
-    $left,
-    $right
-);
-INIT_CODE
-
-    return $initialization_code;
+    return substitute_storage($storage) . ' ' . substitute_type($storage) . ' ' . "$name = " .
+           substitute_initfunc($storage) . "($parent, $value, $left, $right);";
 }
 
