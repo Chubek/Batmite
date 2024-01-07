0ce9a3940a1533fe490cd7a28b8bd1a8caddeac6
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-08 02:24:03 +0330

    finished: tree preprocessor

--- treepp.pl
+++ treepp.pl
@@ -14,48 +14,45 @@ while (<STDIN>) {
 sub process_line {
     my ($line) = @_;
 
-    my $pattern = qr{
-        ^%tree\s*
-        (?:%static|%heap)?\s*
-        %name\s+(\w+)\s*
-        (?:(?:%parent\s+(\w+)\s*)?)?
-        %value\s+([^\s%]+)\s*
-        (?:(?:%left\s+(\w+)\s*)?)?
-        (?:(?:%right\s+(\w+)\s*)?)?$
-    }sx;
+    if (my ($name, $storage, $parent, $value, $left, $right) = parse_line($line)) {
+        $storage //= "%heap";
+        $parent  //= "NULL";
+        $left    //= "NULL";
+        $right   //= "NULL";
 
-    $line =~ s/$pattern/instantiate_tree("$1", "$2", "$3", "$4", "$5", "$6", "$7", "$8")/e;
+        return instantiate_tree($name, $storage, $parent, $value, $left, $right);
+    }
 
     return $line;
 }
 
-sub handle_default {
-    my ($variable, $default) = @_;
-    return defined($variable) && $variable =~ /\S/ ? $variable : $default;
+sub parse_line {
+    my ($line) = @_;
+
+    if ($line =~ /^%tree\s*(?:%(\w+)\s*)?%name\s+(\w+)\s*(?:%parent\s+(\w+)\s*)?%value\s+([^\s%]+)\s*(?:%left\s+(\w+)\s*)?(?:%right\s+(\w+)\s*)?$/) {
+        return ($2, $1, $3, $4, $5, $6);
+    }
+
+    return;
 }
 
 sub substitute_storage {
     my ($storage) = @_;
-    return $storage eq "%static" ? "$PREFIX" . "_STATIC" : "";
+    return $storage eq "static" ? "$PREFIX" . "_STATIC" : "";
 }
 
 sub substitute_type {
     my ($storage) = @_;
-    return $storage eq "%heap" ? "$PREFIX" . "_PTR" : "$PREFIX" . "_NONPTR";
+    return $storage eq "heap" ? "$PREFIX" . "_PTR" : "$PREFIX" . "_NONPTR";
 }
 
 sub substitute_initfunc {
     my ($storage) = @_;
-    return $storage eq "%heap" ? "$PREFIX" . "_ALLOC" : "$PREFIX" . "_NONALLOC";
+    return $storage eq "heap" ? "$PREFIX" . "_ALLOC" : "$PREFIX" . "_NONALLOC";
 }
 
 sub instantiate_tree {
     my ($name, $storage, $parent, $value, $left, $right) = @_;
-
-    $storage = handle_default($storage, "%heap");
-    $parent  = handle_default($parent, "NULL");
-    $left    = handle_default($left, "NULL");
-    $right   = handle_default($right, "NULL");
 
     return substitute_storage($storage) . ' ' . substitute_type($storage) . ' ' . "$name = " .
            substitute_initfunc($storage) . "($parent, $value, $left, $right);";
