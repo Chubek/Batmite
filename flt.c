e9c588026b607c4399bd5b2bc21744fd01a63bae
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-08 06:36:03 +0330

    finished: initial implementation of ieee 475

--- flt.c
+++ flt.c
@@ -2,6 +2,12 @@
 #include <stdint.h>
 #include <math.h>
 
+#define FLOAT32_FRACTION_MASK 0x800000
+#define FLOAT32_BIAS 127
+
+#define FLOAT64_FRACTION_MASK 0x10000000000000
+#define FLOAT64_BIAS 1023
+
 typedef struct {
     uint32_t fraction : 23;
     uint32_t exponent : 8;
@@ -33,13 +39,11 @@ Float64 newFloat64(uint64_t fraction, uint16_t exponent, uint8_t sign) {
 Float32 f32Addition(Float32 a, Float32 b) {
     Float32 result = {0};
 
-    
-    uint32_t mantissa_a = (a.fraction | 0x800000) << 7; 
-    uint32_t mantissa_b = (b.fraction | 0x800000) << 7; 
-    int exponent_a = a.exponent - 127;
-    int exponent_b = b.exponent - 127;
+    uint32_t mantissa_a = (a.fraction | FLOAT32_FRACTION_MASK) << 7;
+    uint32_t mantissa_b = (b.fraction | FLOAT32_FRACTION_MASK) << 7;
+    int exponent_a = a.exponent - FLOAT32_BIAS;
+    int exponent_b = b.exponent - FLOAT32_BIAS;
 
-    
     if (exponent_a < exponent_b) {
         mantissa_a >>= (exponent_b - exponent_a);
         result.exponent = b.exponent;
@@ -48,7 +52,6 @@ Float32 f32Addition(Float32 a, Float32 b) {
         result.exponent = a.exponent;
     }
 
-    
     if (a.sign == b.sign) {
         result.fraction = mantissa_a + mantissa_b;
         result.sign = a.sign;
@@ -62,7 +65,6 @@ Float32 f32Addition(Float32 a, Float32 b) {
         }
     }
 
-    
     if (result.fraction & 0x80000000) {
         result.fraction >>= 1;
         result.exponent++;
@@ -72,7 +74,6 @@ Float32 f32Addition(Float32 a, Float32 b) {
 }
 
 Float32 f32Subtraction(Float32 a, Float32 b) {
-    
     b.sign = !b.sign;
     return f32Addition(a, b);
 }
@@ -80,15 +81,13 @@ Float32 f32Subtraction(Float32 a, Float32 b) {
 Float32 f32Multiplication(Float32 a, Float32 b) {
     Float32 result = {0};
 
-    
-    uint32_t mantissa_a = a.fraction | 0x800000; 
-    uint32_t mantissa_b = b.fraction | 0x800000; 
-    int exponent_a = a.exponent - 127;
-    int exponent_b = b.exponent - 127;
+    uint32_t mantissa_a = a.fraction | FLOAT32_FRACTION_MASK;
+    uint32_t mantissa_b = b.fraction | FLOAT32_FRACTION_MASK;
+    int exponent_a = a.exponent - FLOAT32_BIAS;
+    int exponent_b = b.exponent - FLOAT32_BIAS;
 
-    
     result.fraction = (uint32_t)(((uint64_t)mantissa_a * mantissa_b) >> 23);
-    result.exponent = a.exponent + b.exponent - 127;
+    result.exponent = a.exponent + b.exponent - FLOAT32_BIAS;
     result.sign = a.sign ^ b.sign;
 
     return result;
@@ -97,26 +96,22 @@ Float32 f32Multiplication(Float32 a, Float32 b) {
 Float32 f32Division(Float32 a, Float32 b) {
     Float32 result = {0};
 
-    
-    uint32_t mantissa_a = a.fraction | 0x800000; 
-    uint32_t mantissa_b = b.fraction | 0x800000; 
-    int exponent_a = a.exponent - 127;
-    int exponent_b = b.exponent - 127;
+    uint32_t mantissa_a = a.fraction | FLOAT32_FRACTION_MASK;
+    uint32_t mantissa_b = b.fraction | FLOAT32_FRACTION_MASK;
+    int exponent_a = a.exponent - FLOAT32_BIAS;
+    int exponent_b = b.exponent - FLOAT32_BIAS;
 
-    
     result.fraction = (uint32_t)(((uint64_t)mantissa_a << 23) / mantissa_b);
-    result.exponent = a.exponent - b.exponent + 127;
+    result.exponent = a.exponent - b.exponent + FLOAT32_BIAS;
     result.sign = a.sign ^ b.sign;
 
     return result;
 }
 
 Float32 f32Exponentiation(Float32 a, Float32 b) {
-    
-    
     Float32 result = {0};
     result.fraction = (uint32_t)powf((float)a.fraction, (float)b.fraction);
-    result.exponent = 127;
+    result.exponent = FLOAT32_BIAS;
     result.sign = a.sign ^ b.sign;
     return result;
 }
@@ -124,13 +119,11 @@ Float32 f32Exponentiation(Float32 a, Float32 b) {
 Float64 f64Addition(Float64 a, Float64 b) {
     Float64 result = {0};
 
-    
-    uint64_t mantissa_a = (a.fraction | 0x10000000000000); 
-    uint64_t mantissa_b = (b.fraction | 0x10000000000000); 
-    int exponent_a = a.exponent - 1023;
-    int exponent_b = b.exponent - 1023;
+    uint64_t mantissa_a = (a.fraction | FLOAT64_FRACTION_MASK);
+    uint64_t mantissa_b = (b.fraction | FLOAT64_FRACTION_MASK);
+    int exponent_a = a.exponent - FLOAT64_BIAS;
+    int exponent_b = b.exponent - FLOAT64_BIAS;
 
-    
     if (exponent_a < exponent_b) {
         mantissa_a >>= (exponent_b - exponent_a);
         result.exponent = b.exponent;
@@ -139,8 +132,7 @@ Float64 f64Addition(Float64 a, Float64 b) {
         result.exponent = a.exponent;
     }
 
-    
-    if (a.sgn == b.sign) {
+    if (a.sign == b.sign) {
         result.fraction = mantissa_a + mantissa_b;
         result.sign = a.sign;
     } else {
@@ -153,8 +145,7 @@ Float64 f64Addition(Float64 a, Float64 b) {
         }
     }
 
-    
-    if (result.fraction & 0x10000000000000) {
+    if (result.fractin & FLOAT64_FRACTION_MASK) {
         result.fraction >>= 1;
         result.exponent++;
     }
@@ -163,7 +154,6 @@ Float64 f64Addition(Float64 a, Float64 b) {
 }
 
 Float64 f64Subtraction(Float64 a, Float64 b) {
-    
     b.sign = !b.sign;
     return f64Addition(a, b);
 }
@@ -171,15 +161,13 @@ Float64 f64Subtraction(Float64 a, Float64 b) {
 Float64 f64Multiplication(Float64 a, Float64 b) {
     Float64 result = {0};
 
-    
-    uint64_t mantissa_a = a.fraction | 0x10000000000000; 
-    uint64_t mantissa_b = b.fraction | 0x10000000000000; 
-    int exponent_a = a.exponent - 1023;
-    int exponent_b = b.exponent - 1023;
+    uint64_t mantissa_a = a.fraction | FLOAT64_FRACTION_MASK;
+    uint64_t mantissa_b = b.fraction | FLOAT64_FRACTION_MASK;
+    int exponent_a = a.exponent - FLOAT64_BIAS;
+    int exponent_b = b.exponent - FLOAT64_BIAS;
 
-    
     result.fraction = (uint64_t)(((uint64_t)mantissa_a * mantissa_b) >> 52);
-    result.exponent = a.exponent + b.exponent - 1023;
+    result.exponent = a.exponent + b.exponent - FLOAT64_BIAS;
     result.sign = a.sign ^ b.sign;
 
     return result;
@@ -188,26 +176,23 @@ Float64 f64Multiplication(Float64 a, Float64 b) {
 Float64 f64Division(Float64 a, Float64 b) {
     Float64 result = {0};
 
-    
-    uint64_t mantissa_a = a.fraction | 0x10000000000000; 
-    uint64_t mantissa_b = b.fraction | 0x10000000000000; 
-    int exponent_a = a.exponent - 1023;
-    int exponent_b = b.exponent - 1023;
+    uint64_t mantissa_a = a.fraction | FLOAT64_FRACTION_MASK;
+    uint64_t mantissa_b = b.fraction | FLOAT64_FRACTION_MASK;
+    int exponent_a = a.exponent - FLOAT64_BIAS;
+    int exponent_b = b.exponent - FLOAT64_BIAS;
 
-    
     result.fraction = (uint64_t)(((uint64_t)mantissa_a << 52) / mantissa_b);
-    result.exponent = a.exponent - b.exponent + 1023;
+    result.exponent = a.exponent - b.exponent + FLOAT64_BIAS;
     result.sign = a.sign ^ b.sign;
 
     return result;
 }
 
 Float64 f64Exponentiation(Float64 a, Float64 b) {
-    
-    
     Float64 result = {0};
     result.fraction = (uint64_t)pow((double)a.fraction, (double)b.fraction);
-    result.exponent = 1023;
+    result.exponent = FLOAT64_BIAS;
     result.sign = a.sign ^ b.sign;
     return result;
 }
+o
